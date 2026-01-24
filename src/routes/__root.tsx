1be4833d89a4d7ab14ae591d0170b610fecf10b7
Philipp Gartz <philipp@gartz.dev>
2026-01-24 14:48:38 +0100

    fix: favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -35,6 +35,12 @@ export const Route = createRootRoute({
 				type: "image/svg+xml",
 				href: "/favicon.svg",
 			},
+			{
+				rel: "icon",
+				type: "image/png",
+				sizes: "32x32",
+				href: "/favicon-32x32.png",
+			},
 		],
 	}),
 
